e5a5d65373a6a2a97fc58b1bc8da778a586d38f8
Tien Phan <tienphan1989@gmail.com>
2020-06-03 04:07:26 -0400

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,21 @@
+class Dog
+ 
+    
+   
+    def name=(newname)
+        @name = newname
+    end
+   
+    def name
+      @name
+    end
+
+    def breed=(newbreed)
+        @breed = newbreed
+    end
+     
+    def breed
+        @breed
+    end
+   
+end
---
+++ lib/person.rb
@@ -0,0 +1,21 @@
+class Person
+ 
+    
+   
+    def name=(newname)
+        @name = newname
+    end
+   
+    def name
+      @name
+    end
+
+    def job=(newjob)
+        @job = newjob
+    end
+     
+    def job
+        @job
+    end
+   
+end
